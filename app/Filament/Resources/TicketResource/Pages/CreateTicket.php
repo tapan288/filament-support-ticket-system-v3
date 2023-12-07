f09fc7f1222194b2215a515696595002f28f44cc
Tapan Sharma <tapansharma69@gmail.com>
2023-12-07 23:20:02 +0545

    customize tickets data before saving

--- app/Filament/Resources/TicketResource/Pages/CreateTicket.php
+++ app/Filament/Resources/TicketResource/Pages/CreateTicket.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateTicket extends CreateRecord
 {
     protected static string $resource = TicketResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['assigned_by'] = auth()->id();
+
+        return $data;
+    }
 }
