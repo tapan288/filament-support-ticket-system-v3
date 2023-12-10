2f2e55cde1a9adb696c780e9512d4987d3c5a922
Tapan Sharma <tapansharma69@gmail.com>
2023-12-10 22:05:14 +0545

    replace simple CRUD resources with modal resources

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -81,8 +81,6 @@ class CategoryResource extends Resource
     {
         return [
             'index' => Pages\ListCategories::route('/'),
-            'create' => Pages\CreateCategory::route('/create'),
-            'edit' => Pages\EditCategory::route('/{record}/edit'),
         ];
     }
 }
