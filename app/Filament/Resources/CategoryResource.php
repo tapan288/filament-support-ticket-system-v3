f104398174a1a8582d085faf60d5327851fcff14
Tapan Sharma <tapansharma69@gmail.com>
2023-12-10 21:46:07 +0545

    change icons, group nav items

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -22,7 +22,7 @@ class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-funnel';
 
     public static function form(Form $form): Form
     {
