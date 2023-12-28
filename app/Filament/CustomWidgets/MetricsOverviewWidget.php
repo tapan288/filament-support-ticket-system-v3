90b1f0267f70d7f5ebb99d92276e96e520f46508
Tapan Sharma <tapansharma69@gmail.com>
2023-12-28 21:06:15 +0545

    work on MetricsOverviewWidget

---
+++ app/Filament/CustomWidgets/MetricsOverviewWidget.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\CustomWidgets;
+
+use Filament\Widgets\Widget;
+use Filament\Widgets\Concerns\CanPoll;
+
+class MetricsOverviewWidget extends Widget
+{
+    use CanPoll;
+
+    protected static string $view = 'filament.custom-widgets.metrics-overview-widget';
+
+    /**
+     * @var array<MetricWidget> | null
+     */
+    protected ?array $cachedMetrics = null;
+
+    protected int|string|array $columnSpan = 'full';
+
+    protected function getColumns(): int
+    {
+        $count = count($this->getCachedStats());
+
+        if ($count < 3) {
+            return 3;
+        }
+
+        if (($count % 3) !== 1) {
+            return 3;
+        }
+
+        return 4;
+    }
+
+    /**
+     * @return array<MetricWidget>
+     */
+    protected function getCachedMetrics(): array
+    {
+        return $this->cachedMetrics ??= $this->getMetrics();
+    }
+
+    /**
+     * @return array<MetricWidget>
+     */
+    protected function getMetrics(): array
+    {
+        return [];
+    }
+}
