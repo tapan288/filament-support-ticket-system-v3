2e5e311158e623002ca165841021eee602ba3f83
Tapan Sharma <tapansharma69@gmail.com>
2024-01-07 22:37:15 +0545

    display tickets data

---
+++ app/Livewire/ListTickets.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Livewire;
+
+use Filament\Tables;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Ticket;
+use Livewire\Component;
+use Filament\Tables\Table;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Columns\SelectColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Columns\TextInputColumn;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Tables\Concerns\InteractsWithTable;
+
+class ListTickets extends Component implements HasForms, HasTable
+{
+    use InteractsWithTable;
+    use InteractsWithForms;
+
+    protected static ?string $model = Ticket::class;
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Ticket::query())
+            ->columns([
+                TextColumn::make('title')
+                    ->description(fn(Ticket $record): ?string => $record?->description ?? null)
+                    ->searchable()
+                    ->sortable(),
+                SelectColumn::make('status')
+                    ->options(self::$model::STATUS),
+                TextColumn::make('priority')
+                    ->badge()
+                    ->colors([
+                        'warning' => self::$model::PRIORITY['Medium'],
+                        'success' => self::$model::PRIORITY['Low'],
+                        'danger' => self::$model::PRIORITY['High'],
+                    ]),
+                TextColumn::make('assignedTo.name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('assignedBy.name')
+                    ->searchable()
+                    ->sortable(),
+                TextInputColumn::make('comment'),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                SelectFilter::make('status')
+                    ->options(self::$model::STATUS)
+                    ->placeholder('Filter By Status'),
+                SelectFilter::make('priority')
+                    ->options(self::$model::PRIORITY)
+                    ->placeholder('Filter By Priority'),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public function render()
+    {
+        return view('livewire.list-tickets');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\ListTickets;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('tickets', ListTickets::class)->name('tickets.index');
